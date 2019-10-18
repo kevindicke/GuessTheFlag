cbf4f95269346fe8ef1eccddd7c78fe4cb25e080
Kevin Dicke <k.dicke7@gmail.com>
2019-10-18 12:54:00 -0700

    inital commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack{
+            Color(red: 0, green: 1, blue: 1).edgesIgnoringSafeArea(.all)
+            Text("Some text")
+        }
     }
 }
 
